e652dcae176a9ed78158b7a0a4c6570b12d0cd9b
Zannatul Maria <zannatulfarzana363@gmail.com>
2026-05-22 22:51:11 +0600

    basic array

---
+++ BASIC/array.js
@@ -0,0 +1,8 @@
+const number= [10,44,65,46,96];
+number[1]= 100;
+
+console.log(number);
+console.log("Length: "+number.length);
+
+number.push(200)
+console.log("After pushing new number: "+number);
